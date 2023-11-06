a3925c5959de7b9ba422cdd301bbb6492a7b869b
Steven Li <stevenli159@gmail.com>
2023-11-06 11:07:41 -0500

    clean up, add comments

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import urllib
 import io
 from pygame import mixer
 
+url = "http://www.fromjapan.co.jp/japan/en/mercari/search/yohji/-/?sort_order=new"
+
 # initialize selenium web driver
 chrome_options = webdriver.ChromeOptions() 
 chrome_options.page_load_strategy = "none"
@@ -24,24 +26,24 @@ chrome_options.add_argument('--allow-running-insecure-content')
 driver = webdriver.Chrome(options=chrome_options)
 driver.implicitly_wait(5)
 
-url = "http://www.fromjapan.co.jp/japan/en/mercari/search/yohji/-/?sort_order=new"
-
+# initialize tkinter frame layout
 root = Tk()
 root.title("FromJapan Scraper")
 root.geometry('250x250')
 root.attributes('-topmost', True)
-
 image_button = Button(root)
 image_label = Label(root)
 price_label = Label(root, text="temp", font=("Arial", 14))
 price_label.place(x=0,y=0)
 image_button.place(x=0,y=0)
 
+# import notification sound
 mixer.init()
 notif_sound = mixer.Sound('notification.mp3')
 
 prev_prices = ['','','','',''] # stores the last several prices of items to prevent repetition
 
+# sets tkinter labels to given image and price. Image is a button that redirects to passed link 
 def update_item(img_url, price, link):
     page=urllib.request.Request(img_url,headers={'User-Agent': 'Mozilla/5.0'}) 
     raw_data=urllib.request.urlopen(page).read()
@@ -53,26 +55,23 @@ def update_item(img_url, price, link):
     price_label.config(text=price)
     notif_sound.play()
     
-
+# load the page and refresh app frame
 def refresh():
     driver.get(url)
-    shop_items_xpath = "//div[@class='shop-item flex lg:block lg:w-1/4 mb-6 lg:px-3 justify-center w-full']"
-    elem = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, shop_items_xpath)))
-    # shop_items = driver.find_elements(By.XPATH, shop_items_xpath)
-    # item = shop_items[0]
-    item = driver.find_element(By.XPATH, shop_items_xpath)
-    # print(item.get_attribute('outerHTML'))
+    shop_items_xpath = "//div[@class='shop-item flex lg:block lg:w-1/4 mb-6 lg:px-3 justify-center w-full']" # xpath for one item
+    elem = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, shop_items_xpath))) # waits for element to exist, i.e. page to be loaded
+    item = driver.find_element(By.XPATH, shop_items_xpath) # gets first (most recent) item on the page
     img = item.find_element(By.TAG_NAME, "img").get_attribute('src')
-    price_xpath = "//span[@class='w-full text-black truncate text-xs text-grey mb-2']"
+    price_xpath = "//span[@class='w-full text-black truncate text-xs text-grey mb-2']" # xpath for price label
     price = item.find_element(By.XPATH, price_xpath).get_attribute('innerHTML')
-    if price not in prev_prices:
+    if price not in prev_prices: # make sure item was not recently displayed; necessary as FromJapan display order is not consistent
         prev_prices.append(price)
         prev_prices.pop(0)
-        item_link_xpath = "//a[@class='h-full flex']"
+        item_link_xpath = "//a[@class='h-full flex']" 
         item_link = item.find_element(By.XPATH, item_link_xpath).get_attribute('href')
         update_item(img_url=img, price=price, link=item_link)
 
-    root.after(5000, refresh)
+    root.after(3000, refresh) # rerun refresh page every 3000 ms
 
 refresh()
 root.mainloop()
