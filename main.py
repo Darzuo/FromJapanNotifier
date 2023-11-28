ae5244e33598d6eb0df73eb61a8b4d35001ef377
Steven Li <stevenli159@gmail.com>
2023-11-28 12:33:57 -0500

    fixes

--- main.py
+++ main.py
@@ -73,7 +73,6 @@ def init_driver():
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--allow-running-insecure-content')
     driver = webdriver.Chrome(options=chrome_options)
-    driver.implicitly_wait(5)
 
 # start the notification screen
 def start_notifier():
@@ -104,23 +103,26 @@ def update_item(img_url, price, link, desc, price_label, image_button, desc_labe
 def refresh(price_label, image_button, desc_label):
     driver.get(search_url)
     shop_items_xpath = "//div[@class='shop-item flex lg:block lg:w-1/4 mb-6 lg:px-3 justify-center w-full']" # xpath for one item
-    elem = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, shop_items_xpath))) # waits for element to exist, i.e. page to be loaded
-    item = driver.find_element(By.XPATH, shop_items_xpath) # gets first (most recent) item on the page
-    img = item.find_element(By.TAG_NAME, "img").get_attribute('src')
-    price_xpath = "//span[@class='w-full text-black truncate text-xs text-grey mb-2']" # xpath for price label
-    price = item.find_element(By.XPATH, price_xpath).get_attribute('innerHTML')
-    # print(item.get_attribute('outerHTML'))
-    desc_xpath = "//a[@class='inline-block w-full text-black truncate text-sm']"
-    desc = item.find_element(By.XPATH, desc_xpath).get_attribute('innerHTML')
-    if (translate):
-        desc = GoogleTranslator(source='auto', target='english').translate(desc)
-    if price not in prev_prices: # make sure item was not recently displayed; necessary as FromJapan display order is not consistent
-        prev_prices.append(price)
-        prev_prices.pop(0)
-        item_link_xpath = "//a[@class='h-full flex']" 
-        item_link = item.find_element(By.XPATH, item_link_xpath).get_attribute('href')
-        update_item(img, price, item_link, desc, price_label, image_button, desc_label)
-
+    try:
+        elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, shop_items_xpath))) # waits for element to exist, i.e. page to be loaded
+        item = driver.find_element(By.XPATH, shop_items_xpath) # gets first (most recent) item on the page
+        img = item.find_element(By.TAG_NAME, "img").get_attribute('src')
+        price_xpath = "//span[@class='w-full text-black truncate text-xs text-grey mb-2']" # xpath for price label
+        price = item.find_element(By.XPATH, price_xpath).get_attribute('innerHTML')
+        # print(item.get_attribute('outerHTML'))
+        desc_xpath = "//a[@class='inline-block w-full text-black truncate text-sm']"
+        desc = item.find_element(By.XPATH, desc_xpath).get_attribute('innerHTML')
+        if (translate):
+            desc = GoogleTranslator(source='auto', target='english').translate(desc)
+        if price not in prev_prices: # make sure item was not recently displayed; necessary as FromJapan display order is not consistent
+            prev_prices.append(price)
+            prev_prices.pop(0)
+            item_link_xpath = "//a[@class='h-full flex']" 
+            item_link = item.find_element(By.XPATH, item_link_xpath).get_attribute('href')
+            update_item(img, price, item_link, desc, price_label, image_button, desc_label)
+    except:
+        pass
+    
     root.after(1000 * refresh_freq, lambda: refresh(price_label, image_button, desc_label)) # rerun refresh page every 3000 ms
 
 prev_prices = ['','','','',''] # stores the last several prices of items to prevent repetition due to FromJapan inconsistency
